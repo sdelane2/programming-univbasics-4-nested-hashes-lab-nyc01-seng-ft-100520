ff9cb1170af585227ccabc41d865c6add86263c9
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-19 18:14:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,12 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+
+programmer_hash
+
 end
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
